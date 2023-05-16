a48f3e211f87775e48e93158090cdbfe43620613
Melike Sena Çakır <115591143+melikesenacakir@users.noreply.github.com>
2023-05-16 10:56:24 +0300

    Update main.java

--- javaExample-1/main.java
+++ javaExample-1/main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package main;
 
-/**
- *
- * @author Melike
- */
 public class main {
 
     public static void main(String[] args) {
